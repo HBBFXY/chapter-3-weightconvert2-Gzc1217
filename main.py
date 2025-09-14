26836583c93267ff9de8f2d5e3889d1d72e9ddd2
Gzc1217 <3208074303@qq.com>
2025-09-14 10:47:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-# 在这个文件里编写代码
+initial_weight = float(input("请输入您在的球上的初始体重："))
+for year in range(1,11):
+    earth_weigh = initial_weight + year * 0.5
+    moon_weight = earth_weigh * 0.165
+    print(f"第{year}年，地球上的体重：{earth_weigh:.2f} 公斤，月球上的体重：{moon_weight:.2f}")
